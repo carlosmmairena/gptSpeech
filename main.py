69e0f7b43f330b000f6e92226f17d87088a410fb
Carlos Mairena <carlosmiguelmairena@gmail.com>
2023-04-21 14:39:26 -0600

    fix: cli lines

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ from playsound import playsound
 # Listen the voice from mic
 def recognize_speech_from_mic(recognizer: speechrecog.Recognizer, microphone: speechrecog.Microphone):
     with microphone as source:
-        print("Ajustando micrófono al ruido del ambiente...")
-        print("Espera...")
+        print("----------- Ajustando micrófono al ruido del ambiente...")
+        print("----------- ...")
+        print("----------- ...")
         recognizer.adjust_for_ambient_noise(source, duration=2)
-        print("Habla...")
+        print("----------- Prepárate para hablar...")
+        print("----------- Dime algo:")
         audio = recognizer.listen(source)
         return audio.get_wav_data()
+
 
 # Recognize the audio from mic to whisper to get the text
 def recognize_speech_with_whisper(audio_data):
@@ -25,17 +28,18 @@ def recognize_speech_with_whisper(audio_data):
         temp_audio_file.write(audio_data)
         temp_audio_file.flush()
 
-        print(temp_audio_file.name)
-        model = whisper.load_model("base")
+        model      = whisper.load_model("base")
         transcript = model.transcribe(temp_audio_file.name)
 
     return transcript["text"]
+
 
 # Send to chatgpt to get a completion
 def send_to_completion(text : str):
     response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=100)
     text_response = response["choices"][0]["text"]
     return text_response
+
 
 # Use the Google Text Voice to generate audio for the text completion and reproduce it
 def text_to_voice(text : str):
@@ -70,7 +74,8 @@ def main():
                 is_running = False
             else:
                 response_by_ai = send_to_completion(audio_transcripted)
-                print("AI Te ha respondido: {}".format(response_by_ai))
+                print("----------------------------------------------")
+                print("--- AI Te ha respondido: {}".format(response_by_ai))
                 text_to_voice(response_by_ai)
         else:
             print("No te hemos entendido...")
